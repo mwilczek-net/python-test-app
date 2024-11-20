512fd6bfdbec7c2155f0509099f29967873a47bd
null <null>
2024-11-20 13:26:04 +0100

    Use numlib

--- core/processing.py
+++ core/processing.py
@@ -1,6 +1,10 @@
 from pythontestlib.strings import format
+from numlib import converters
 
 def process() -> str:
     return format.skip_indentation("""
     This is a text
     """)
+
+def number_list() -> list[int]:
+    return [converters.opposit(x) for x in range(11)]
--- core/processing_test.py
+++ core/processing_test.py
@@ -1,6 +1,9 @@
-from .processing import process
+from .processing import process, number_list
 
 def test_process() -> None:
     result = process()
     assert isinstance(result, str)
     assert result.startswith('This')
+
+def test_number_list() -> None:
+    assert number_list() == [-0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
